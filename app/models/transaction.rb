2863daade69ede5c66903c30afb388c36497d061
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2022-09-13 13:33:29 -0300

    add model validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+  validates :bank_fee, :old_balance, :new_balance, :amount, presence: true
   # validates :type, inclusion: { in: ['Depósito', 'Saque', 'Transferências entre Contas', 'Extrato'],
   #   message: "%{value} não é uma operação válida" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
   has_many :transactions
+  validates :email, :full_name, presence: true
   acts_as_paranoid
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   before_create do
     self.account_number = "0000#{User.last.account_number.to_i + 1}" if User.count.positive?
   end
+
 
   validates :cpf, uniqueness: true
   validate :validate_cpf
